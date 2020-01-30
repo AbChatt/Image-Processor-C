0e74197f4dcc5aa96325cee9b41fcdc71ac3adaf
AbChatt <abhi1506@live.com>
2020-01-30 04:22:58 -0500

    Moved bulk of scanf code to iterator() function to better follow SOLID design and use design patterns

--- image_processing.c
+++ image_processing.c
@@ -4,7 +4,171 @@
  * Read an image from the standard input and set the red value of each pixel to
  * zero.
  */
+
+void iterator(int *col_num, int *row_num, int *max_col_val) {
+   char header;
+   //int r_pixel;
+   //int g_pixel;
+   //int b_pixel;
+   //int col_num;
+   //int row_num;
+   //int max_col_val;
+
+   // first, get rid of P3 line
+
+   // need to preserve line to check output
+
+   //scanf("%s %s", &header, &header[1]);
+   //header[2] = '\0';
+   //printf("%s\n", header);
+
+   printf("P3\n");
+   scanf("%c\n", &header);
+   getchar();
+
+   // now, read the dimensions of the file
+
+   scanf("%d %d", col_num, row_num);
+   //getchar();
+   printf("%d %d\n", *col_num, *row_num);
+
+   // now, get max colour value
+
+   scanf("%d", max_col_val);
+   //getchar();
+   printf("%d\n", *max_col_val);
+
+   // now, set the parameters for our loop using these values
+   
+   // for (int i = 0; i < row_num; i++) {
+   //    for (int j = 0; j < col_num; j++) {
+   //       scanf("%d %d %d", &r_pixel, &g_pixel, &b_pixel);
+   //       //getchar();
+         
+   //       r_pixel = 0;
+   //       printf("%d %d %d  ", r_pixel, g_pixel, b_pixel);
+   //    }
+
+   //    printf("\n");
+   // }
+}
+
 void remove_red()
+{
+   //char header;
+   int r_pixel;
+   int g_pixel;
+   int b_pixel;
+   int col_num = 0;
+   int row_num = 0;
+   int max_col_val = 0;
+
+   // first, get rid of P3 line
+
+   // need to preserve line to check output
+
+   //scanf("%s %s", &header, &header[1]);
+   //header[2] = '\0';
+   //printf("%s\n", header);
+
+   //printf("P3\n");
+   //scanf("%c\n", &header);
+   //getchar();
+
+   // now, read the dimensions of the file
+
+   //scanf("%d %d", &col_num, &row_num);
+   //getchar();
+   //printf("%d %d\n", col_num, row_num);
+
+   // now, get max colour value
+
+   //scanf("%d", &max_col_val);
+   //getchar();
+   //printf("%d\n", max_col_val);
+
+   iterator(&col_num, &row_num, &max_col_val);
+
+   // now, set the parameters for our loop using these values
+   
+   for (int i = 0; i < row_num; i++) {
+      for (int j = 0; j < col_num; j++) {
+         scanf("%d %d %d", &r_pixel, &g_pixel, &b_pixel);
+         //getchar();
+         
+         r_pixel = 0;
+         printf("%d %d %d  ", r_pixel, g_pixel, b_pixel);
+      }
+
+      printf("\n");
+   }
+   
+}
+
+/*
+ * Read an image from the standard input and convert it from colour to black and wh,ite.
+ */
+void convert_to_black_and_white()
+{
+   //char header;
+   int r_pixel;
+   int g_pixel;
+   int b_pixel;
+   int col_num = 0;
+   int row_num = 0;
+   int max_col_val = 0;
+   int avg;
+
+   // first, get rid of P3 line
+
+   // need to preserve line to check output
+
+   // scanf("%c", &header);
+   // getchar();
+   // scanf("%c", &header);
+   // getchar();
+   // printf("%c\n", header);
+
+   //printf("P3\n");
+   //scanf("%c\n", &header);
+   //getchar();
+
+   // now, read the dimensions of the file
+
+   //scanf("%d %d", &col_num, &row_num);
+   //getchar();
+   //printf("%d %d\n", col_num, row_num);
+
+   // now, get max colour value
+
+   //scanf("%d", &max_col_val);
+   //getchar();
+   //printf("%d\n", max_col_val);
+
+   iterator(&col_num, &row_num, &max_col_val);
+
+   // now, set the parameters for our loop using these values
+   
+   for (int i = 0; i < row_num; i++) {
+      for (int j = 0; j < col_num; j++) {
+         scanf("%d %d %d", &r_pixel, &g_pixel, &b_pixel);
+         getchar();
+         
+         avg = (r_pixel + g_pixel + b_pixel) / 3;
+         r_pixel = avg;
+         g_pixel = avg;
+         b_pixel = avg;
+         printf("%d %d %d  ", r_pixel, g_pixel, b_pixel);
+      }
+
+      printf("\n");
+   }
+}
+
+/*
+ * Read an image from the standard input and convert it to a square image.
+ */
+void instagram_square()
 {
    char header;
    int r_pixel;
@@ -51,72 +215,5 @@ void remove_red()
 
       printf("\n");
    }
-   
-}
-
-/*
- * Read an image from the standard input and convert it from colour to black and wh,ite.
- */
-void convert_to_black_and_white()
-{
-   char header;
-   int r_pixel;
-   int g_pixel;
-   int b_pixel;
-   int col_num;
-   int row_num;
-   int max_col_val;
-   int avg;
-
-   // first, get rid of P3 line
-
-   // need to preserve line to check output
-
-   // scanf("%c", &header);
-   // getchar();
-   // scanf("%c", &header);
-   // getchar();
-   // printf("%c\n", header);
-
-   printf("P3\n");
-   scanf("%c\n", &header);
-   getchar();
-
-   // now, read the dimensions of the file
-
-   scanf("%d %d", &col_num, &row_num);
-   getchar();
-   printf("%d %d\n", col_num, row_num);
-
-   // now, get max colour value
-
-   scanf("%d", &max_col_val);
-   getchar();
-   printf("%d\n", max_col_val);
-
-   // now, set the parameters for our loop using these values
-   
-   for (int i = 0; i < row_num; i++) {
-      for (int j = 0; j < col_num; j++) {
-         scanf("%d %d %d", &r_pixel, &g_pixel, &b_pixel);
-         getchar();
-         
-         avg = (r_pixel + g_pixel + b_pixel) / 3;
-         r_pixel = avg;
-         g_pixel = avg;
-         b_pixel = avg;
-         printf("%d %d %d  ", r_pixel, g_pixel, b_pixel);
-      }
-
-      printf("\n");
-   }
-}
-
-/*
- * Read an image from the standard input and convert it to a square image.
- */
-void instagram_square()
-{
- 
 }
 
