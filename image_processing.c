0668f49d37d5a8e61ed9e556c071834a9549d829
AbChatt <abhi1506@live.com>
2020-01-31 23:21:54 -0500

    Fixed issues with printing out P3 header and significantly revamped instagram_square() code as previous implementation only addressed 1 case. Also added more sample files to test

--- image_processing.c
+++ image_processing.c
@@ -6,13 +6,12 @@
  */
 
 void iterator(int *col_num, int *row_num, int *max_col_val) {
-   char header;
+   char header[3];
 
    // need to preserve P3 line to check output
 
-   printf("P3\n");
-   scanf("%c\n", &header);
-   getchar();
+   scanf("%s\n", header);
+   printf("%s\n", header);
 
    // now, read the dimensions of the file
 
@@ -27,29 +26,49 @@ void iterator(int *col_num, int *row_num, int *max_col_val) {
 
 void remove_red()
 {
-   //char header;
    int r_pixel;
    int g_pixel;
    int b_pixel;
    int col_num = 0;
    int row_num = 0;
    int max_col_val = 0;
+   int count = 0;
 
    iterator(&col_num, &row_num, &max_col_val);
 
    // now, set the parameters for our loop using these values
    
-   for (int i = 0; i < row_num; i++) {
-      for (int j = 0; j < col_num; j++) {
-         scanf("%d %d %d", &r_pixel, &g_pixel, &b_pixel);
-         
-         r_pixel = 0;
-         printf("%d %d %d  ", r_pixel, g_pixel, b_pixel);
-      }
+   if (col_num > 10) {
+      for (int i = 0; i < row_num; i++) {
+         for (int j = 0; j < col_num; j++) {
+            scanf("%d %d %d", &r_pixel, &g_pixel, &b_pixel);
+            
+            r_pixel = 0;
+            printf("%d %d %d  ", r_pixel, g_pixel, b_pixel);
+            count++;
 
-      printf("\n");
+            if (count > 9) {
+               printf("\n");
+               count = 0;
+            }
+         }
+
+         printf("\n");
+      }
    }
-   
+   else
+   {
+      for (int i = 0; i < row_num; i++) {
+         for (int j = 0; j < col_num; j++) {
+            scanf("%d %d %d", &r_pixel, &g_pixel, &b_pixel);
+            
+            r_pixel = 0;
+            printf("%d %d %d  ", r_pixel, g_pixel, b_pixel);
+         }
+
+         printf("\n");
+      }
+   }
 }
 
 /*
@@ -57,7 +76,6 @@ void remove_red()
  */
 void convert_to_black_and_white()
 {
-   //char header;
    int r_pixel;
    int g_pixel;
    int b_pixel;
@@ -65,24 +83,50 @@ void convert_to_black_and_white()
    int row_num = 0;
    int max_col_val = 0;
    int avg;
+   int count = 0;
 
    iterator(&col_num, &row_num, &max_col_val);
 
    // now, set the parameters for our loop using these values
-   
-   for (int i = 0; i < row_num; i++) {
-      for (int j = 0; j < col_num; j++) {
-         scanf("%d %d %d", &r_pixel, &g_pixel, &b_pixel);
-         getchar();
-         
-         avg = (r_pixel + g_pixel + b_pixel) / 3;
-         r_pixel = avg;
-         g_pixel = avg;
-         b_pixel = avg;
-         printf("%d %d %d  ", r_pixel, g_pixel, b_pixel);
-      }
 
-      printf("\n");
+   if (col_num > 10) {
+      for (int i = 0; i < row_num; i++) {
+         for (int j = 0; j < col_num; j++) {
+            scanf("%d %d %d", &r_pixel, &g_pixel, &b_pixel);
+            getchar();
+         
+            avg = (r_pixel + g_pixel + b_pixel) / 3;
+            r_pixel = avg;
+            g_pixel = avg;
+            b_pixel = avg;
+            printf("%d %d %d  ", r_pixel, g_pixel, b_pixel);
+            count++;
+
+            if (count > 9) {
+               printf("\n");
+               count = 0;
+            }
+         }
+
+         printf("\n");
+      }
+   }
+   else
+   {
+      for (int i = 0; i < row_num; i++) {
+         for (int j = 0; j < col_num; j++) {
+            scanf("%d %d %d", &r_pixel, &g_pixel, &b_pixel);
+            getchar();
+         
+            avg = (r_pixel + g_pixel + b_pixel) / 3;
+            r_pixel = avg;
+            g_pixel = avg;
+            b_pixel = avg;
+            printf("%d %d %d  ", r_pixel, g_pixel, b_pixel);
+         }
+
+         printf("\n");
+      }
    }
 }
 
@@ -91,47 +135,111 @@ void convert_to_black_and_white()
  */
 void instagram_square()
 {
-   char header;
+   char header[3];
    int r_pixel;
    int g_pixel;
    int b_pixel;
    int col_num;
    int row_num;
    int max_col_val;
+   int count = 0;
+   int level = 1;
 
    // need to preserve P3 line to check output
 
-   printf("P3\n");
-   scanf("%c\n", &header);
-   getchar();
+   scanf("%s\n", header);
+   printf("%s\n", header);
 
    // now, read the dimensions of the file
 
    scanf("%d %d", &col_num, &row_num);
 
-   if (col_num > row_num) {
-      col_num = row_num;
-   }
-   else if (col_num < row_num) {
-      row_num = col_num;
-   }
-   
-   printf("%d %d\n", col_num, row_num);
-
-   // now, get max colour value
-
-   scanf("%d", &max_col_val);
-   printf("%d\n", max_col_val);
+   // process dimensions as needed
 
    // now, set the parameters for our loop using these values
-   
-   for (int i = 0; i < row_num; i++) {
-      for (int j = 0; j < col_num; j++) {
-         scanf("%d %d %d", &r_pixel, &g_pixel, &b_pixel);
-         printf("%d %d %d  ", r_pixel, g_pixel, b_pixel);
-      }
 
-      printf("\n");
+   if (col_num > row_num) {
+      col_num = row_num;
+
+      printf("%d %d\n", row_num, row_num);
+
+      // now, get max colour value
+
+      scanf("%d", &max_col_val);
+      printf("%d\n", max_col_val);
+
+      if (col_num > 10) {
+         for (int i = 0; i < row_num; i++) {
+            for (int j = 0; j < col_num; j++) {
+               scanf("%d %d %d", &r_pixel, &g_pixel, &b_pixel);
+               printf("%d %d %d  ", r_pixel, g_pixel, b_pixel);
+               count++;
+
+               if (count > 9) {
+               printf("\n");
+               count = 0;
+               }
+            }
+
+            // get to newline, ignoring all other characters
+            while ((getchar()) != '\n');
+            printf("\n");  
+         }
+      }
+      else
+      {
+         for (int i = 0; i < row_num; i++) {
+            for (int j = 0; j < col_num; j++) {
+               scanf("%d %d %d", &r_pixel, &g_pixel, &b_pixel);
+               printf("%d %d %d  ", r_pixel, g_pixel, b_pixel);
+            }
+
+            // get to newline, ignoring all other characters
+
+            while ((getchar()) != '\n');
+            printf("\n");
+         }
+      }
    }
+   else if (col_num <= row_num) {
+      row_num = col_num;
+
+      printf("%d %d\n", col_num, row_num);
+
+      // now, get max colour value
+
+      scanf("%d", &max_col_val);
+      printf("%d\n", max_col_val);
+
+      // now, set the parameters for our loop using these values
+
+      if (col_num > 10) {
+         for (int i = 0; i < row_num; i++) {
+            for (int j = 0; j < col_num; j++) {
+               scanf("%d %d %d", &r_pixel, &g_pixel, &b_pixel);
+               printf("%d %d %d  ", r_pixel, g_pixel, b_pixel);
+               count++;
+
+               if (count > 9) {
+                  printf("\n");
+                  count = 0;
+               }
+            }
+
+            printf("\n");
+         }
+      }  
+      else
+      {
+         for (int i = 0; i < row_num; i++) {
+            for (int j = 0; j < col_num; j++) {
+               scanf("%d %d %d", &r_pixel, &g_pixel, &b_pixel);
+               printf("%d %d %d  ", r_pixel, g_pixel, b_pixel);
+            }
+
+            printf("\n");
+         }
+      }
+   }   
 }
 
